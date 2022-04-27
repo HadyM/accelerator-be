fd7836ac23be57b4c244802184192a3ed905565a
Hady Mohamed <hadymohamed@pursuit.org>
2022-04-27 15:48:54 -0400

    installed pg-promise created db folder with config, seed, & schema files

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // DEPENDENCIES
+const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
 const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
 app.get("/", (req, res) => {
