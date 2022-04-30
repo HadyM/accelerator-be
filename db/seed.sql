21c37e4461480bcb5add7370e8d2169d03bb45b8
Hady Mohamed <hadymohamed@pursuit.org>
2022-04-29 20:02:58 -0400

    set outline for seed.sql file

---
+++ db/seed.sql
@@ -0,0 +1,29 @@
+INSERT INTO recipes
+(title, image, description, time, ingredients, instructions)
+VALUES
+(),
+();
+
+INSERT INTO cut_styles
+(name, image, description)
+VALUES
+(),
+();
+
+INSERT INTO knives
+(name, image, description)
+VALUES
+(),
+();
+
+INSERT INTO pots_pans
+(name, image, description)
+VALUES
+(),
+();
+
+INSERT INTO cooking_styles
+(name, image, description)
+VALUES
+(),
+();
